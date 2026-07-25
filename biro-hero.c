e875ccfb86ba9b03756eb2f4298d43927ad5bad6
Stephen M. Cameron <stephenmcameron@gmail.com>
2026-07-25 14:05:56 -0400

    clean up level images
    Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>

--- biro-hero.c
+++ biro-hero.c
@@ -7,8 +7,8 @@
 
 #include "png_utils.h"
 
-#define WINDOW_WIDTH 1024
-#define WINDOW_HEIGHT 768
+#define WINDOW_WIDTH (1024 * 1.2)
+#define WINDOW_HEIGHT (768 * 1.2)
 #define TARGET_FPS 60
 #define FRAME_TARGET_TIME (1000 / TARGET_FPS)
 
